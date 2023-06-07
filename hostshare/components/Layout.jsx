44ac708c4ce341ac19f5a928241297373c7f8f2b
lomick2090 <lomick2090@gmail.com>
2023-06-07 16:59:46 -0500

    Continue nav bar development

--- hostshare/components/Layout.jsx
+++ hostshare/components/Layout.jsx
@@ -1,5 +1,7 @@
 import Link from 'next/link'
 import listings from '../data/listings.json'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
 export default function Layout({children}) {
 
@@ -18,21 +20,24 @@ export default function Layout({children}) {
 
     return (
         <div>
-            <nav className="container sticky top-0 bg-default-color h-20 min-w-full flex items-center shadow-lg z-20 p-8">
-                <Link href="/"><img src='../images/hostshareBlack.png' className="h-14 w-120"/></Link> 
-                <div className="w-[550px] h-8 border-[1px] rounded-xl mr-auto ml-auto bg-[white] flex items-center justify-between">
-                    <select className="rounded-xl w-32 pl-2" name="city">
-                        <option value="null">Choose City</option>
-                        {listingCities}
-                    </select>
-                    <input type="date" placeholder='Keywords' className="rounded-xl w-32 pl-2"/> 
-                    -
-                    <input type="date" placeholder='Keywords' className="rounded-xl w-32 pl-2"/>
-                    <input type="number" placeholder='# of guests' className="rounded-xl w-28 pl-2"/>
-                </div>
+            <nav className="sticky top-0 bg-default-color sm:h-20 h-36 min-w-full flex sm:flex-row flex-col sm:items-center sm:justify-around shadow-lg z-20 pr-8 pl-8 flex-shrink-0">
+                <Link href="/" className="ml-auto mr-auto"><img src='../images/hostshareBlack.png' className="h-14 w-120"/></Link> 
+        <div className="flex justify-center">
+            <div className="w-[550px] h-8 border-[1px] rounded-xl bg-[white] flex items-center justify-between">
+                <select className="rounded-xl w-32 pl-2" name="city">
+                    <option value="null">Choose City</option>
+                    {listingCities}
+                </select>
+                <input type="date" placeholder='Keywords' className="rounded-xl w-32 pl-2"/>
+                -
+                <input type="date" placeholder='Keywords' className="rounded-xl w-32 pl-2"/>
+                <input type="number" placeholder='# of guests' className="rounded-xl w-28 pl-2"/>
+            </div>
+            <div className="bg-[red] rounded-full h-8 w-8 flex-shrink-0 flex justify-center items-center cursor-pointer"><FontAwesomeIcon className="text-[white]" icon={faSearch}></FontAwesomeIcon></div>
+        </div>
 
-                <div className="w-24 h-8 border-[1px] rounded-xl mr-auto ml-auto bg-[white] flex items-center justify-between">
-                    <img src="../public/images/profpic.png" className="w-24" />
+                <div className="w-24 m-2 rounded-xl mr-auto ml-auto flex items-center justify-between">
+                    <img src="../images/profilepic.jpg" className="w-8 rounded-full" />
                 </div>
             </nav>
             <div className="flex flex-col items-center">
